7aa061eb99aec9d877f9eccdd1404ea78a41e45d
tomClearyRAC <tom.cleary@rac.com.au>
2021-12-08 20:07:41 +0800

    add small ncurses program to Program.cs that displays a->g and then closes

--- MazeSearch/Program.cs
+++ MazeSearch/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Mindmagma.Curses;
+
+var Screen = NCurses.InitScreen();
+NCurses.NoDelay(Screen, true);
+NCurses.NoEcho();
+
+for (var i = 'a'; i < 'h'; i++)
+{
+    NCurses.Erase();
+    NCurses.AddChar(i);
+    System.Threading.Thread.Sleep(500);
+    NCurses.Refresh();
+}
+System.Threading.Thread.Sleep(3000);
+NCurses.EndWin();
