e09f5d507dec64cf5603f68d9fc7d2900a2386e4
Tom Cleary <tom.cleary@rac.com.au>
2021-12-21 12:08:48 +0800

    reorder methods alphabetically and remove an unnecessary if and bool from addGoal()

--- MazeSearch/Maze.cs
+++ MazeSearch/Maze.cs
@@ -20,20 +20,39 @@ public class Maze
         GenerateNewOpenMaze();
     }
 
-    /// <summary> Set this.maze to a new maze with no Wall tiles inserted </summary>
-    public void GenerateNewOpenMaze()
-    {
-       int[,] newMaze = new int[numRows, numCols]; 
 
-       for (int rowNum = 0; rowNum < numRows; rowNum++)
-       {
-           for (int colNum = 0; colNum < numCols; colNum++)
-           {
-               newMaze[rowNum, colNum] = (int) MazeTileNum.Undiscovered;
-           }
-       }
-       this.maze = newMaze;
+    /// <summary> Add the starting position of the search to the maze </summary>
+    public void AddCurrentPosition()
+    {
+        throw new NotImplementedException();
     }
+
+
+    /// <summary> Add a goal point at a random location in the maze for the search alogirthm to find </summary>
+    public void AddGoal()
+    {
+        bool undiscoveredFound = IsUndiscoveredTile();
+
+        if (!undiscoveredFound)
+        {
+            throw new InvalidOperationException("There is no available tile to place the goal onto.");
+        }
+
+        int randRow;
+        int randCol;
+        // Find a random undiscovered tile to make the goal of the search
+        do
+        {
+            randRow = rand.Next(this.numRows);
+            randCol = rand.Next(this.numCols);
+        }
+        while (!(this.maze[randRow, randCol] == (int) MazeTileNum.Undiscovered));
+
+        this.maze[randRow, randCol] = (int) MazeTileNum.Goal;
+
+        this.goal = (randRow, randCol);
+    }
+
 
     /// <summary> Add walls to this.maze to meet the specified density </summary>
     public void AddWalls(double density)
@@ -55,43 +74,22 @@ public class Maze
         }
     }
 
-    /// <summary> Add a goal point at a random location in the maze for the search alogirthm to find </summary>
-    public void AddGoal()
+
+    /// <summary> Set this.maze to a new maze with no Wall tiles inserted </summary>
+    public void GenerateNewOpenMaze()
     {
-        bool undiscoveredFound = IsUndiscoveredTile();
+       int[,] newMaze = new int[numRows, numCols]; 
 
-        if (!undiscoveredFound)
-        {
-            throw new InvalidOperationException("There is no available tile to place the goal onto.");
-        }
-
-        bool locationFound = false;
-        int randRow;
-        int randCol;
-
-        // Find a random undiscovered tile to make the goal of the search
-        do
-        {
-            randRow = rand.Next(this.numRows);
-            randCol = rand.Next(this.numCols);
-
-            if (this.maze[randRow, randCol] == (int) MazeTileNum.Undiscovered)
-            {
-                locationFound = true;
-            }
-        }
-        while (!locationFound);
-
-        this.maze[randRow, randCol] = (int) MazeTileNum.Goal;
-
-        this.goal = (randRow, randCol);
+       for (int rowNum = 0; rowNum < numRows; rowNum++)
+       {
+           for (int colNum = 0; colNum < numCols; colNum++)
+           {
+               newMaze[rowNum, colNum] = (int) MazeTileNum.Undiscovered;
+           }
+       }
+       this.maze = newMaze;
     }
 
-    /// <summary> Add the starting position of the search to the maze </summary>
-    public void AddCurrentPosition()
-    {
-        throw new NotImplementedException();
-    }
 
     /// <summary> Check if there is an undiscovered tile in the maze </summary>
     public bool IsUndiscoveredTile()
@@ -111,4 +109,6 @@ public class Maze
         }
         return false;
     }
+
+
 }
