93d3f22afd846cd9d9addd7e1dabca7b0ce7b68d
Tom Cleary <tom.cleary@rac.com.au>
2021-12-21 15:49:00 +0800

    add Search class and unit test class

--- MazeSearch/Search.cs
+++ MazeSearch/Search.cs
@@ -1,8 +1,40 @@
 
 public static class Search
 {
-    public static void Test()
+    public static void BFS(Maze maze)
     {
-           
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+
+        queue.Enqueue((maze.startingPosition.row, maze.startingPosition.column));
+
+        (int Row, int Col) previousNode = (Constants.InvalidPosition, Constants.InvalidPosition);
+
+        bool goalFound   = false;
+        bool goalInQueue = false;
+
+        int movesMade = 0;
+
+        while (queue.Count > 0 && !goalFound)
+        {
+            if (previousNode != (Constants.InvalidPosition, Constants.InvalidPosition))
+            {
+                maze.maze[previousNode.Row, previousNode.Col] = (int) MazeTileNum.Discovered;
+            }
+
+            (int Row, int Col) currentNode = queue.Dequeue();
+
+            if (maze.maze[currentNode.Row, currentNode.Col] == (int) MazeTileNum.Goal)
+            {
+                goalFound = true;
+            }
+
+            maze.maze[currentNode.Row, currentNode.Col] = (int) MazeTileNum.CurrentPosition;
+
+            if (movesMade > 0 && previousNode != (Constants.InvalidPosition, Constants.InvalidPosition))
+            {
+                return;
+            }
+        }
+
     }
 }
---
+++ MazeSearchTest/SearchTest.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace MazeSearchTest;
+
+public class SearchTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Template()
+    {
+        Assert.Fail();
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+    
+}
